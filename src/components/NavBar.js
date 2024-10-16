bfeb5b6ef169b99bf951cfe81806639ec9ac325f
YHA76 <yahyabnlpro@gmail.com>
2024-10-16 18:57:15 +0200

    add social network

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,58 +1,50 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom'; // Utilisation de NavLink
-import '../styles/NavBar.css'; 
+import { NavLink } from 'react-router-dom';
+import '../styles/NavBar.css'; // Importation du fichier CSS
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareSnapchat, faSquareInstagram } from '@fortawesome/free-brands-svg-icons'; // Importation de l'icône Snapchat
 
 function Navbar() {
   return (
     <nav className="navbar">
       <ul className="navList">
         <li className="navItem">
-          <NavLink 
-            to="/" 
-            className="navLink"
-            activeClassName="active"
-            exact
-          >
+          <NavLink to="/" className="navLink" exact>
             Accueil
           </NavLink>
         </li>
         <li className="navItem">
-          <NavLink 
-            to="/about" 
-            className="navLink"
-            activeClassName="active"
-          >
+          <NavLink to="/about" className="navLink">
             Qui nous sommes
           </NavLink>
         </li>
         <li className="navItem">
-          <NavLink 
-            to="/seances" 
-            className="navLink"
-            activeClassName="active"
-          >
+          <NavLink to="/seances" className="navLink">
             Séances
           </NavLink>
         </li>
         <li className="navItem">
-          <NavLink 
-            to="/programmes" 
-            className="navLink"
-            activeClassName="active"
-          >
+          <NavLink to="/programmes" className="navLink">
             Programmes
           </NavLink>
         </li>
         <li className="navItem">
-          <NavLink 
-            to="/inscrire" 
-            className="navLink"
-            activeClassName="active"
-          >
+          <NavLink to="/inscrire" className="navLink">
             S'inscrire
           </NavLink>
         </li>
       </ul>
+      <div className="socialIcons">
+        <FontAwesomeIcon 
+          icon={faSquareSnapchat} 
+          beat 
+          style={{color: "#fffc00",}} />
+        <FontAwesomeIcon
+          icon={faSquareInstagram}
+          beat
+          style={{color: "#ff0285",}} />
+        </div>
+
     </nav>
   );
 }
