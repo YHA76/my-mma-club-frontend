87bf47d264bba4c3f4429e59db892d7163e9572c
YHA76 <yahyabnlpro@gmail.com>
2024-11-08 20:34:58 +0100

    add picture (#3)
    * add picture
    
    * add picture
    
    * add picture
    
    * add picture
    
    * add picture
    
    * add picture

---
+++ src/views/Gallery.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Navbar from '../components/NavBar';
+import { motion } from 'framer-motion'; // Importation de framer-motion pour l'animation
+import "../styles/styles.css"; 
+import "../styles/gallery.css"; 
+
+function Gallery() {
+  // Images et descriptions associées
+  const images = [
+    {
+      src: require("../assets/images/picture_3.JPG"),
+      description: ( <> Stage avec Benoît Saint-Denis dans notre salle : <br /> 18/12/2023</> )
+    },
+    {
+      src: require("../assets/images/picture_2.jpg"),
+      description: ( <> Première séance avec notre coach de MMA et lutte ​Ibrahim et son ami Islam champion de Grappling : <br /> 10/11/2023 </> )
+    },
+    {
+      src: require("../assets/images/picture_1.jpeg"),
+      description: ( <> remier entraînement depuis la reprise, avec Yazid notre coach de Muay thaï et Striking : <br /> 11/11/2023 </>)
+    }
+  ];
+
+  return (
+    <div>
+      <Navbar />
+      <div className="gallery-page" style={{ paddingTop: '90px' }}>
+        <h1 className="gallery-title">Galerie Photos</h1>
+        <div className="gallery-container">
+          {images.map((image, index) => (
+            <div key={index} className="gallery-item">
+              <h3 className="gallery-description">{image.description}</h3>
+              {/* Animation de grossissement au survol avec framer-motion */}
+              <motion.img 
+                src={image.src} 
+                alt={`Description ${index + 1}`} 
+                whileHover={{ scale: 1.1 }} /* Grossissement */
+                whileTap={{ scale: 0.95 }}  /* Rétrécissement au clic */
+                transition={{ duration: 0.3 }} /* Durée de la transition */
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Gallery;
--- src/views/Inscrire.js
+++ src/views/Inscrire.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import Navbar from '../components/NavBar'; // Importation de la barre de menu
-import "../styles/styles.css";
+import Navbar from '../components/NavBar'; // Barre de navigation
+import "../styles/styles.css"; // Importation des styles globaux
+import "../styles/inscription.css"; // Importation des styles spécifiques
 
-function Inscrire() {
+function Inscription() {
+  // Tableau d'images et de descriptions
+  const pages = [
+    { src: require('../assets/images/Licence-2025-MMA-1.jpg'), description: "Première page du formulaire d'inscription" },
+    { src: require('../assets/images/Licence-2025-MMA-2.jpg'), description: "Deuxième page du formulaire d'inscription" },
+    { src: require('../assets/images/Licence-2025-MMA-3.jpg'), description: "Troisième page du formulaire d'inscription" }
+  ];
+
   return (
     <div>
-            <Navbar />
-            <div style={{ paddingTop: '90px' }}>
-      <h1>S'inscrire</h1>
-      <p>Inscrivez-vous à notre club de MMA.</p>
-    </div>
+      <Navbar />
+      <div className="inscription-page" style={{ paddingTop: '90px' }}>
+        <h1 className="inscription-title">Formulaire d'Inscription</h1>
+        
+        {/* Affichage des images sous forme de galerie */}
+        <div className="pdf-gallery">
+          {pages.map((page, index) => (
+            <div key={index} className="pdf-gallery-item">
+              <img src={page.src} alt={`Page ${index + 1} du formulaire d'inscription`} className="pdf-image" />
+              <p className="pdf-description">{page.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Inscrire;
+export default Inscription;
