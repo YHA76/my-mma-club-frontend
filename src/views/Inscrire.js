8317fd8e7c9d0b5ad98067ffd07fd2eaf7a9daad
YHA76 <yahyabnlpro@gmail.com>
2024-11-08 20:21:17 +0100

    add picture

--- src/views/Inscrire.js
+++ src/views/Inscrire.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import Navbar from '../components/NavBar'; // Importation de la barre de menu
-import "../styles/styles.css";
+import Navbar from '../components/NavBar'; // Barre de navigation
+import "../styles/styles.css"; // Importation des styles globaux
+import "../styles/inscription.css"; // Importation des styles spécifiques
 
-function Inscrire() {
+// Importation des images des pages du PDF
+import page1 from '../assets/images/Licence 2025 MMA-1.jpg';
+import page2 from '../assets/images/Licence 2025 MMA-2.jpg';
+import page3 from '../assets/images/Licence 2025 MMA-3.jpg';
+
+function Inscription() {
+  // Tableau d'images et de descriptions
+  const pages = [
+    { src: page1, description: "Première page du formulaire d'inscription" },
+    { src: page2, description: "Deuxième page du formulaire d'inscription" },
+    { src: page3, description: "Troisième page du formulaire d'inscription" }
+  ];
+
   return (
     <div>
-            <Navbar />
-            <div style={{ paddingTop: '90px' }}>
-      <h1>S'inscrire</h1>
-      <p>Inscrivez-vous à notre club de MMA.</p>
-    </div>
+      <Navbar />
+      <div className="inscription-page" style={{ paddingTop: '90px' }}>
+        <h1 className="inscription-title">Formulaire d'Inscription</h1>
+        
+        {/* Affichage des images sous forme de galerie */}
+        <div className="pdf-gallery">
+          {pages.map((page, index) => (
+            <div key={index} className="pdf-gallery-item">
+              <img src={page.src} alt={`Page ${index + 1} du formulaire d'inscription`} className="pdf-image" />
+              <p className="pdf-description">{page.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Inscrire;
+export default Inscription;
