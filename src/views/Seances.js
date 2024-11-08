fc957143c076e95ed8577f9e58305d10d969ae0b
YHA76 <yahyabnlpro@gmail.com>
2024-11-08 22:58:32 +0100

    update navbar

--- src/views/Seances.js
+++ src/views/Seances.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/NavBar';
 import Schedule from '../components/Schedule.tsx';
 import "../styles/styles.css";
 import "../styles/seances.css"; 
 
 function Seances() {
+  const [filter, setFilter] = useState("all"); // État pour le filtre d'âge
+
+  const handleFilterChange = (ageGroup) => {
+    setFilter(ageGroup); // Mettre à jour l'état en fonction du filtre sélectionné
+  };
+
   return (
     <div>
       <Navbar />
@@ -16,6 +22,15 @@ function Seances() {
           </p>
         </div>
 
+        {/* Boutons de filtrage */}
+        <div className="seances-filter-buttons">
+          <button onClick={() => handleFilterChange("all")}>Tous</button>
+          <button onClick={() => handleFilterChange("adults")}>Adultes</button>
+          <button onClick={() => handleFilterChange("teens")}>Ados</button>
+          <button onClick={() => handleFilterChange("children")}>Enfants</button>
+        </div>
+
+        {/* Contenu du planning filtré */}
         <div className="seances-schedule-container">
           <div className="seances-schedule-grid">
             <div className="seances-time-header"></div>
@@ -23,44 +38,45 @@ function Seances() {
               <div key={day} className="seances-day-header">{day}</div>
             ))}
 
-            <div className="seances-age-group">Adultes (18+)</div>
-            <Schedule time="20h00 - 22h00" activity="Muay Thai" type="muay-thai" />
-            <Schedule />
-            <Schedule time="20h00 - 22h15" activity="Grappling" type="grappling" />
-            <Schedule />
-            <Schedule time="20h00 - 22h15" activity="MMA" type="mma" />
-            <Schedule time="15h00 - 16h30" activity="Boxe Anglaise" type="boxing" />
-            <Schedule />
+            {/* Filtrage en fonction de l'âge */}
+            {filter === "adults" || filter === "all" ? (
+              <>
+                <div className="seances-age-group">Adultes (18+)</div>
+                <Schedule time="20h00 - 22h00" activity="Muay Thai" type="muay-thai" />
+                <Schedule />
+                <Schedule time="20h00 - 22h15" activity="Grappling" type="grappling" />
+                <Schedule />
+                <Schedule time="20h00 - 22h15" activity="MMA" type="mma" />
+                <Schedule time="15h00 - 16h30" activity="Boxe Anglaise" type="boxing" />
+                <Schedule />
+              </>
+            ) : null}
 
-            <div className="seances-age-group">Ados (13-17 ans)</div>
-            <Schedule time="18h00 - 19h30" activity="Kickboxing" type="kickboxing" />
-            <Schedule />
-            <Schedule time="18h00 - 19h30" activity="Boxe Anglaise" type="boxing" />
-            <Schedule />
-            <Schedule time="18h00 - 19h30" activity="Lutte" type="wrestling" />
-            <Schedule />
-            <Schedule />
+            {filter === "teens" || filter === "all" ? (
+              <>
+                <div className="seances-age-group">Ados (13-17 ans)</div>
+                <Schedule time="18h00 - 19h30" activity="Kickboxing" type="kickboxing" />
+                <Schedule />
+                <Schedule time="18h00 - 19h30" activity="Boxe Anglaise" type="boxing" />
+                <Schedule />
+                <Schedule time="18h00 - 19h30" activity="Lutte" type="wrestling" />
+                <Schedule />
+                <Schedule />
+              </>
+            ) : null}
 
-            <div className="seances-age-group">Enfants (-13 ans)</div>
-            <Schedule />
-            <Schedule />
-            <Schedule />
-            <Schedule />
-            <Schedule />
-            <Schedule />
-            <Schedule time="11h00 - 12h15" activity="MMA" type="mma" />
-          </div>
-        </div>
-
-        <div className="seances-legend">
-          <h3>Légende des cours</h3>
-          <div className="seances-legend-items">
-            <div className="seances-legend-item muay-thai">Muay Thaî</div>
-            <div className="seances-legend-item grappling">Grappling</div>
-            <div className="seances-legend-item mma">MMA</div>
-            <div className="seances-legend-item boxing">Boxe Anglaise</div>
-            <div className="seances-legend-item kickboxing">Kickboxing</div>
-            <div className="seances-legend-item wrestling">Lutte</div>
+            {filter === "children" || filter === "all" ? (
+              <>
+                <div className="seances-age-group">Enfants (-13 ans)</div>
+                <Schedule />
+                <Schedule />
+                <Schedule />
+                <Schedule />
+                <Schedule />
+                <Schedule />
+                <Schedule time="11h00 - 12h15" activity="MMA" type="mma" />
+              </>
+            ) : null}
           </div>
         </div>
       </div>
