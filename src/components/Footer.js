3ab0a588cc58fe8745bc9cce0527650ce739ae48
YHA76 <yahyabnlpro@gmail.com>
2024-11-11 17:06:01 +0100

    fix name files + add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,42 @@
+// Footer.js
 import React from 'react';
 import '../styles/Footer.css';
 
-const Footer = () => {
+function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <h3>Nous Contacter</h3>
-        <p>Vous avez des questions ? N'hésitez pas à nous contacter !</p>
-        <a href="mailto:MMACASBAHFIGHT.PRESIDENT@OUTLOOK.FR" className="contact-link">
-        MMACASBAHFIGHT.PRESIDENT@OUTLOOK.FR
-        </a>
+        <div className="footer-section contact-info">
+          <h3>Contactez-nous</h3>
+          <a href="mailto:MMACASBAHFIGHT.PRESIDENT@OUTLOOK.FR" className="contact-link">
+          MMACASBAHFIGHT.PRESIDENT@OUTLOOK.FR</a>
+          <p>Téléphone : 06 18 02 58 23</p>
+          <p>Adresse : Maison du plateau Pl. du châtelet, 76000 Rouen</p>
+        </div>
+        
+        <div className="footer-section main-links">
+          <h3>Liens Principaux</h3>
+          <ul>
+            <li><a href="/">Accueil</a></li>
+            <li><a href="/about">Qui nous sommes</a></li>
+            <li><a href="/gallery">Galerie</a></li>
+            <li><a href="/seances">Séances</a></li>
+            <li><a href="inscrire">S'inscrire</a></li>
+          </ul>
+        </div>
+
+        <div className="footer-section logo">
+          <h3>Nom du Site</h3>
+          <p>MMA Casbah Fight</p>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        <p>&copy; {new Date().getFullYear()} MMA Casbah Fight. Tous droits réservés.</p>
+        <p>Crédits : Design et Développement par Benali Yahya</p>
       </div>
     </footer>
   );
-};
+}
 
 export default Footer;
