c25d6a906be36236dc4de13dc3c3ca91d326264a
YHA76 <yahyabnlpro@gmail.com>
2025-02-03 12:26:17 +0100

    add error 404

--- src/views/NotFound.js
+++ src/views/NotFound.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const NotFound = () => {
+function NotFound() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>404 - Page Not Found</h1>
@@ -14,6 +14,6 @@ const NotFound = () => {
       </Link>
     </div>
   );
-};
+}
 
 export default NotFound;
